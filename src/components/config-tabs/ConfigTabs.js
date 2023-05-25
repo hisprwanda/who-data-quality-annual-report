d2d4ff3163fb07c8ce00199a57b4aaf0299962ef
Maurice Jules M <julesmaurice.mulisa@gmail.com>
2023-05-25 13:38:18 +0200

    added missing relations layouts and save changes btn for quality parameters

--- src/components/config-tabs/ConfigTabs.js
+++ src/components/config-tabs/ConfigTabs.js
@@ -301,7 +301,124 @@ function Tabs() {
         <div className={toggleState === 3 ? "content  active-content" : "content"} >
           <p>Numerator Relations</p>
           <hr />
-          <p>Configure relavant numerator Relations</p>
+
+            <div class="relationsContainer">
+            <Table>
+                <TableHead>
+                    <TableRowHead>
+                        <TableCellHead>Name</TableCellHead>
+                        <TableCellHead>Numerator A</TableCellHead>
+                        <TableCellHead>Numerator B</TableCellHead>
+                        <TableCellHead>Type</TableCellHead>
+                        <TableCellHead>Threshold (%)</TableCellHead>
+                        <TableCellHead>Threshold explanation</TableCellHead>
+                        <TableCellHead>Description</TableCellHead>
+                        <TableCellHead>Actions</TableCellHead>
+                    </TableRowHead>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                      <TableCell>ANC 1 - TT1 ratio</TableCell>
+                      <TableCell>ANC First standard visit 1st trimester</TableCell>
+                      <TableCell>ANC TT 1given</TableCell>
+                      <TableCell>A ≈ B</TableCell>
+                      <TableCell>5</TableCell>
+                      <TableCell>% difference between A and B.</TableCell>
+                      <TableCell>A and B should be roughly equal</TableCell>
+                      <TableCell>
+                        <Button
+                            name="Primary button" onClick={() => window.alert('It works!')} 
+                            basic button value="default" icon={<IconEdit16 />}> Edit
+                        </Button>
+                        <Button
+                            name="Primary button" onClick={() => window.alert('It works!')} 
+                            destructive button value="default" icon={<IconDelete16 />}> Delete
+                        </Button>
+                      </TableCell>
+                  </TableRow>
+                  
+                  <TableRow>
+                      <TableCell>ANC 1 - TT1 ratio</TableCell>
+                      <TableCell>ANC First standard visit 1st trimester</TableCell>
+                      <TableCell>ANC TT 1given</TableCell>
+                      <TableCell>A ≈ B</TableCell>
+                      <TableCell>5</TableCell>
+                      <TableCell>% difference between A and B.</TableCell>
+                      <TableCell>A and B should be roughly equal</TableCell>
+                      <TableCell>
+                        <Button
+                            name="Primary button" onClick={() => window.alert('It works!')} 
+                            basic button value="default" icon={<IconEdit16 />}> Edit
+                        </Button>
+                        <Button
+                            name="Primary button" onClick={() => window.alert('It works!')} 
+                            destructive button value="default" icon={<IconDelete16 />}> Delete
+                        </Button>
+                      </TableCell>
+                  </TableRow>
+                  
+                  <TableRow>
+                      <TableCell>ANC 1 - TT1 ratio</TableCell>
+                      <TableCell>ANC First standard visit 1st trimester</TableCell>
+                      <TableCell>ANC TT 1given</TableCell>
+                      <TableCell>A ≈ B</TableCell>
+                      <TableCell>5</TableCell>
+                      <TableCell>% difference between A and B.</TableCell>
+                      <TableCell>A and B should be roughly equal</TableCell>
+                      <TableCell>
+                        <Button
+                            name="Primary button" onClick={() => window.alert('It works!')} 
+                            basic button value="default" icon={<IconEdit16 />}> Edit
+                        </Button>
+                        <Button
+                            name="Primary button" onClick={() => window.alert('It works!')} 
+                            destructive button value="default" icon={<IconDelete16 />}> Delete
+                        </Button>
+                      </TableCell>
+                  </TableRow>
+                  
+                  <TableRow>
+                      <TableCell>ANC 1 - TT1 ratio</TableCell>
+                      <TableCell>ANC First standard visit 1st trimester</TableCell>
+                      <TableCell>ANC TT 1given</TableCell>
+                      <TableCell>A ≈ B</TableCell>
+                      <TableCell>5</TableCell>
+                      <TableCell>% difference between A and B.</TableCell>
+                      <TableCell>A and B should be roughly equal</TableCell>
+                      <TableCell>
+                        <Button
+                            name="Primary button" onClick={() => window.alert('It works!')} 
+                            basic button value="default" icon={<IconEdit16 />}> Edit
+                        </Button>
+                        <Button
+                            name="Primary button" onClick={() => window.alert('It works!')} 
+                            destructive button value="default" icon={<IconDelete16 />}> Delete
+                        </Button>
+                      </TableCell>
+                  </TableRow>
+                  
+                  <TableRow>
+                      <TableCell>ANC 1 - TT1 ratio</TableCell>
+                      <TableCell>ANC First standard visit 1st trimester</TableCell>
+                      <TableCell>ANC TT 1given</TableCell>
+                      <TableCell>A ≈ B</TableCell>
+                      <TableCell>5</TableCell>
+                      <TableCell>% difference between A and B.</TableCell>
+                      <TableCell>A and B should be roughly equal</TableCell>
+                      <TableCell>
+                        <Button
+                            name="Primary button" onClick={() => window.alert('It works!')} 
+                            basic button value="default" icon={<IconEdit16 />}> Edit
+                        </Button>
+                        <Button
+                            name="Primary button" onClick={() => window.alert('It works!')} 
+                            destructive button value="default" icon={<IconDelete16 />}> Delete
+                        </Button>
+                      </TableCell>
+                  </TableRow>
+                  
+                </TableBody>
+            </Table>
 
             </div>
         </div>
@@ -489,6 +606,25 @@ function Tabs() {
                         <DropdownButton component={<FlyoutMenu><MenuItem label="Increasing" /><MenuItem label="Decreasing" /></FlyoutMenu>} name="buttonName" value="buttonValue" > Overall result </DropdownButton>
                       </TableCell>
                   </TableRow>
+
+                  
+
+                  {/* Save button */}
+
+                  <TableRow>
+                        <TableCell></TableCell>
+                        <TableCell></TableCell>
+                        <TableCell></TableCell>
+                        <TableCell></TableCell>
+                        <TableCell></TableCell>
+                        <TableCell></TableCell>
+                        <TableCell> 
+                          <Button
+                              name="Primary button" onClick={() => window.alert('It works!')} 
+                              primary button value="default" icon={<IconAdd16 />}> Save Changes
+                          </Button>
+                        </TableCell>
+                    </TableRow>
                 </TableBody>
             </Table>
 
