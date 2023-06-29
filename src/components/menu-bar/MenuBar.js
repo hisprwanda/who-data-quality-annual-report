74c3dc06c5f230558fa98977ed99a9254d0bee06
Joseph MANZI <mjos12002@gmail.com>
2023-06-29 16:35:37 +0200

    Implemented modification on the menu to put it to the left side

--- src/components/menu-bar/MenuBar.js
+++ src/components/menu-bar/MenuBar.js
@@ -7,7 +7,23 @@ const MenuBar = () => {
   return (
     <div className='menubarContainer'>
       <div className='logoContainer'>
-        <div>
+
+        <ul>
+          <li>
+            <img src={logo} alt="Image" className='logoImage' />
+          </li>
+          <li>
+            WHO Data Quality Annual Report
+          </li>
+          <li>
+            <NavLink to="/">Annual Report</NavLink>
+            </li>
+            <li>
+              <NavLink to="/configurations">Configurations</NavLink>
+            </li>
+        </ul>
+
+        {/* <div>
           <img src={logo} alt="Image" className='logoImage' />
         </div>
         <div>
@@ -24,7 +40,7 @@ const MenuBar = () => {
               <NavLink to="/configurations">Configurations</NavLink>
             </li>
           </ul>
-        </nav>
+        </nav> */}
       </div>
     </div>
 
