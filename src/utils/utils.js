0cbb596a545b994bb106645a59c63f85eb8d9bf1
Maurice Jules M <julesmaurice.mulisa@gmail.com>
2023-09-07 15:05:50 +0200

    created a structured json objects from table formatted data from dhis2 analytics

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,6 @@
+// import mappedConfigurations from '../data/sample_configuration_map.json'
+// import reportQueryResponse from '../data/sample_response_section_1.json'
+
 export const generateNumeratorCode = (numerators) => {
     if (numerators.length == 0) {
         return 'C' + '1'
@@ -51,12 +54,11 @@ export const getConfigObjectsForAnalytics = (configurations, groupCode) => {
         }
     })
 
-     // Create an object to index datasets by their IDs
-     const indexedNumerators = {};
-     numeratorsInGroup.forEach((numerator) => {
-        indexedNumerators[numerator.dataID] = numerator;
-    });
-
+    // Create an object to index datasets by their IDs
+    const indexedNumerators = {}
+    numeratorsInGroup.forEach((numerator) => {
+        indexedNumerators[numerator.dataID] = numerator
+    })
 
     // Convert the Set to an array to match dataset IDs
     const allDatasetIDs = [...uniqueDatasetIDs]
@@ -67,15 +69,144 @@ export const getConfigObjectsForAnalytics = (configurations, groupCode) => {
     })
 
     // Create an object to index datasets by their IDs
-    const indexedDatasets = {};
+    const indexedDatasets = {}
 
     datasets.forEach((dataset) => {
-        indexedDatasets[dataset.id] = dataset;
-    });
+        indexedDatasets[dataset.id] = dataset
+    })
 
     const configsObj = {
         dataElementsAndIndicators: indexedNumerators,
         dataSets: indexedDatasets,
     }
     return configsObj
+}
+
+export const getReportSectionsData = (
+    reportQueryResponse,
+    mappedConfigurations
+) => {
+    if (!reportQueryResponse || !mappedConfigurations) {
+        return {}
+    }
+
+    const reportSectionsData = {
+        section1: {
+            section1A: [
+                getCompletenessOfFacilityReporting(
+                    reportQueryResponse.reporting_rate_over_all_org_units,
+                    reportQueryResponse.reporting_rate_by_org_unit_level
+                ), // list of objects for every dataset selected (regarding completeness)
+            ],
+        },
+        section2: {
+            section2A: [
+                getTimelinessOfFacilityReporting(
+                    reportQueryResponse.reporting_timeliness_over_all_org_units,
+                    reportQueryResponse.reporting_timeliness_by_org_unit_level
+                ), // list of objects for every dataset selected (regarding timeliness)
+            ],
+        },
+    }
+}
+
+const getCompletenessOfFacilityReporting = (
+    reporting_rate_over_all_org_units,
+    reporting_rate_by_org_unit_level
+) => {
+    // perform data manupulation
+    const completenessOfFacilityReporting = {
+        Section1: {
+            SubsectionA: [
+                {
+                    name: 'HMIS - Reporting rate',
+                    threshold: '90%',
+                    score: '96.1%',
+                    divergentRegionsCount: 1,
+                    divergentRegionsPercent: '25%',
+                    Name: 'Region A',
+                },
+            ],
+        },
+    }
+
+    // Extract key data from the analytics response
+    const headers = reporting_rate_over_all_org_units.headers
+    const rows = reporting_rate_over_all_org_units.rows
+    const metaData = reporting_rate_over_all_org_units.metaData
+
+    // object to store the  data transformed from tabular form to a regural json objects
+    const transformedData = {}
+    for (const row of rows) {
+        const rowData = {}
+
+        // loop through the "headers" array and map values to keys
+        for (let i = 0; i < headers.length; i++) {
+            const header = headers[i]
+            const columnName = header.name
+            const columnValue = row[i]
+
+            // Add the key-value pair to the row data object
+            rowData[columnName] = columnValue
+
+            // in the lines below we relly on the metadata object returned from DHIS2 analytics in which we query the definisions of some uids or values
+            // i.e: row[1] = lZsCb6y0KDX which is a uid of the ou, hence use .name to retrieve its name in the metadata object
+            rowData['region'] = metaData.items[row[1]].name
+            rowData['dataset_name'] = metaData.items[row[0]].name
+        }
+
+        // Use a unique identifier as the key in the transformed data object
+        const uniqueIdentifier = row.join('_') // You can choose a better identifier if available
+        transformedData[uniqueIdentifier] = rowData
+    }
+
+    // The resulting transformed data object will have keys based on header names
+    //   console.log('transformed reporting rates:', transformedData);
+
+    const restructuredData = {}
+
+    for (const key in transformedData) {
+        const entry = transformedData[key]
+        const dx = entry.dx
+        const pe = entry.pe
+
+        if (!restructuredData[dx]) {
+            restructuredData[dx] = {}
+        }
+
+        if (!restructuredData[dx][pe]) {
+            restructuredData[dx][pe] = []
+        }
+
+        restructuredData[dx][pe].push({
+            dataset_name: entry.dataset_name,
+            region: entry.region,
+            ou: entry.ou,
+            score: entry.value,
+            threshold: '90%',
+        })
+    }
+
+    console.log('restructured data:', restructuredData)
+}
+
+const getTimelinessOfFacilityReporting = (
+    reporting_timeliness_over_all_org_units,
+    reporting_timeliness_by_org_unit_level
+) => {
+    // perform data manupulation
+    const completenessOfFacilityReporting = {
+        Section1: {
+            SubsectionA: [
+                {
+                    name: 'HMIS - Reporting rate',
+                    threshold: '90%',
+                    score: '96.1%',
+                    divergentRegionsCount: 1,
+                    divergentRegionsPercent: '25%',
+                    Name: 'Region A',
+                },
+            ],
+        },
+    }
 }
