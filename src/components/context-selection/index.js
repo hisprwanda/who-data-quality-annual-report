9c7adec64676c4434a9f111837ce8f7fd66ea4d0
Kai Vandivier <kpvandivier@gmail.com>
2023-10-31 13:49:26 +0100

    refactor: abstract out selector bar to own file

--- src/components/context-selection/index.js
+++ src/components/context-selection/index.js
@@ -1,3 +1,1 @@
-export { GroupSelector } from './GroupSelector.js'
-export { PeriodSelector } from './PeriodSelector.js'
-export { OrgUnitSelector } from './OrgUnitSelector.js'
+export { ReportParameterSelector } from './ReportParameterSelector.js'
