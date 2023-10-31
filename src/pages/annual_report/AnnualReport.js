d63431f03c104dfeb06ce3fbd302d81e1c9aea89
Kai Vandivier <kpvandivier@gmail.com>
2023-10-31 13:56:40 +0100

    refactor: rename context-selection to report-parameter-selector

--- src/pages/annual_report/AnnualReport.js
+++ src/pages/annual_report/AnnualReport.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { ReportData } from '../../components/annual-report/report-data/ReportData.js'
-import { ReportParameterSelector } from '../../components/context-selection/index.js'
 import MenuBar from '../../components/menu-bar/MenuBar.js'
+import { ReportParameterSelector } from '../../components/report-parameter-selector/index.js'
 
 const Report = () => {
     const [reportParameters, setReportParameters] = useState({})
