829b2f340c094ef67f829b859a92d493286740d4
Kai Vandivier <kpvandivier@gmail.com>
2023-11-08 16:17:55 +0100

    fix: add in refetch again

--- src/utils/configurations/useSetUpConfigurations.js
+++ src/utils/configurations/useSetUpConfigurations.js
@@ -1,5 +1,5 @@
 import { useDataEngine } from '@dhis2/app-runtime'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import defaultConfigurations from './defaultConfigurations.json'
 
 // If in a dev environment, use a custom data store key if it's set.
@@ -26,44 +26,50 @@ export const useSetUpConfigurations = (setConfigurations) => {
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(false)
 
-    useEffect(() => {
-        engine
-            // attempt to fetch configurations
-            .query(CONFIGURATIONS_QUERY)
-            // return configurations object to next handlers
-            .then((data) => data.configurations)
-            .catch((err) => {
-                // if the fetch failed, we might need to set up the data store
-                const { details } = err
-                const needToSetUpDatastore =
-                    details?.httpStatusCode === 404 &&
-                    /Key .* not found in namespace/.test(details?.message)
-                if (needToSetUpDatastore) {
-                    console.log('Setting up data store key', DATASTORE_KEY)
-                    return (
-                        engine
-                            .mutate(SET_UP_CONFIGURATIONS_MUTATION)
-                            // return 'defaultConfigurations' to the next handlers
-                            .then(() => defaultConfigurations)
-                    )
-                } else {
-                    // otherwise, throw the error to the next handler
-                    throw err
-                }
-            })
-            // if we got configurations, either from the fetch or the mutation,
-            // set them
-            .then((configurations) => {
-                setConfigurations(configurations)
-                setLoading(false)
-            })
-            // if there's still an error after attempting to set up the store,
-            // handle that
-            .catch((err) => {
-                setError(err)
-                setLoading(false)
-            })
+    const fetchAndSetConfigurations = useCallback(() => {
+        return (
+            engine
+                // attempt to fetch configurations
+                .query(CONFIGURATIONS_QUERY)
+                // return configurations object to next handlers
+                .then((data) => data.configurations)
+                .catch((err) => {
+                    // if the fetch failed, we might need to set up the data store
+                    const { details } = err
+                    const needToSetUpDatastore =
+                        details?.httpStatusCode === 404 &&
+                        /Key .* not found in namespace/.test(details?.message)
+                    if (needToSetUpDatastore) {
+                        console.log('Setting up data store key', DATASTORE_KEY)
+                        return (
+                            engine
+                                .mutate(SET_UP_CONFIGURATIONS_MUTATION)
+                                // return 'defaultConfigurations' to the next handlers
+                                .then(() => defaultConfigurations)
+                        )
+                    } else {
+                        // otherwise, throw the error to the next handler
+                        throw err
+                    }
+                })
+                // if we got configurations, either from the fetch or the mutation,
+                // set them
+                .then((configurations) => {
+                    setConfigurations(configurations)
+                    setLoading(false)
+                })
+                // if there's still an error after attempting to set up the store,
+                // handle that
+                .catch((err) => {
+                    setError(err)
+                    setLoading(false)
+                })
+        )
     }, [engine, setConfigurations])
 
-    return { loading, error }
+    useEffect(() => {
+        fetchAndSetConfigurations()
+    }, [fetchAndSetConfigurations])
+
+    return { loading, error, refetch: fetchAndSetConfigurations }
 }
