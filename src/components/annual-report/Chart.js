af6330078017ddd96b8d972d29afafb0e3c81cd9
Kai Vandivier <kpvandivier@gmail.com>
2023-11-29 14:30:04 +0100

    fix: have charts reflow on print

--- src/components/annual-report/Chart.js
+++ src/components/annual-report/Chart.js
@@ -4,10 +4,21 @@ import { generateChart } from './generateChart.js'
 
 export const Chart = ({ sectionId, chartId, chartInfo, className }) => {
     const canvasId = `${sectionId}-${chartId}`
+    const chartRef = React.useRef()
 
     useEffect(() => {
-        generateChart(sectionId, canvasId, chartInfo)
+        chartRef.current = generateChart(sectionId, canvasId, chartInfo)
     }, [sectionId, canvasId, chartInfo])
+
+    useEffect(() => {
+        if (!window.matchMedia) {
+            return
+        }
+        const mql = window.matchMedia('print')
+        const onChange = () => chartRef.current.reflow()
+        mql.addEventListener('change', onChange)
+        return () => mql.removeEventListener('change', onChange)
+    }, [])
 
     return <div id={canvasId} className={className} />
 }
